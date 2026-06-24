4f62fb81fb931a9d309d498d0828bb0999debe72
chuckwxy <144194226+chuckwxy@users.noreply.github.com>
2026-06-24 19:03:50 +0800

    Update Douban.java

--- app/src/main/java/com/github/catvod/spider/Douban.java
+++ app/src/main/java/com/github/catvod/spider/Douban.java
@@ -29,8 +29,8 @@ public class Douban extends Spider {
     }
 
     @Override
-    public void init(String extend) throws Exception {
-        this.extend = extend;
+    public void init(Context context) throws Exception {
+        this.extend = context;
     }
 
     @Override
