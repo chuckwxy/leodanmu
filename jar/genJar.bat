46f8ef0af2c1840379bf4563a163b7a469580a6a
chuckwxy <144194226+chuckwxy@users.noreply.github.com>
2026-06-25 13:01:01 +0800

    Update genJar.bat

--- jar/genJar.bat
+++ jar/genJar.bat
@@ -16,8 +16,9 @@ if not exist "%~dp0\spider.jar\smali\org\slf4j\" md "%~dp0\spider.jar\smali\org\
 move "%~dp0\Smali_classes\smali\com\github\catvod\spider" "%~dp0\spider.jar\smali\com\github\catvod\"
 move "%~dp0\Smali_classes\smali\com\github\catvod\js" "%~dp0\spider.jar\smali\com\github\catvod\"
 move "%~dp0\Smali_classes\smali\com\github\catvod\net" "%~dp0\spider.jar\smali\com\github\catvod\"
-move "%~dp0\Smali_classes\com\github\catvod\utils\okhttp" "%~dp0\spider.jar\smali\com\github\catvod\utils\"
 move "%~dp0\Smali_classes\smali\org\slf4j" "%~dp0\spider.jar\smali\org\slf4j\"
+if not exist "%~dp0\spider.jar\smali\com\github\catvod\utils\" mkdir "%~dp0\spider.jar\smali\com\github\catvod\utils\"
+move "%~dp0\Smali_classes\smali\com\github\catvod\utils\okhttp" "%~dp0\spider.jar\smali\com\github\catvod\utils\"
 
 **:: 复制 assets 文件夹到 spider.jar 目录**
 if exist "%~dp0\assets\" (
